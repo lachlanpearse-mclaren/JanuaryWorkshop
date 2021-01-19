ccf2777c1c021c36066e719779783ce27e9fd9ae
Lachlan Pearse <lachlan.pearse@mclaren.com>
2021-01-19 12:02:43 +0000

    Added increase quality and decrease quality methods

--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -6,25 +6,32 @@ class GildedRose(object):
     def update_quality(self):
         for item in self.items:
             self.update_item(item)
+
+    def increase_quality(self,item):
+        item.quality = item.quality + 1
+    
+    def decrease_quality(self,item):
+        item.quality = item.quality - 1
     
     def update_item(self,item):
 
         item_decreases_in_value = item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+        item_has_dynamic_quality = item.name != "Sulfuras, Hand of Ragnaros"
 
         if item_decreases_in_value:
                 if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                    if item_has_dynamic_quality:
+                        self.decrease_quality(item)
         else:
             if item.quality < 50:
-                item.quality = item.quality + 1
+                self.increase_quality(item)
                 if item.name == "Backstage passes to a TAFKAL80ETC concert":
                     if item.sell_in < 11:
                         if item.quality < 50:
-                            item.quality = item.quality + 1
+                            self.increase_quality(item)
                     if item.sell_in < 6:
                         if item.quality < 50:
-                            item.quality = item.quality + 1
+                            self.increase_quality(item)
         if item.name != "Sulfuras, Hand of Ragnaros":
             item.sell_in = item.sell_in - 1
         if item.sell_in < 0:
@@ -32,9 +39,9 @@ class GildedRose(object):
                 if item.name != "Backstage passes to a TAFKAL80ETC concert":
                     if item.quality > 0:
                         if item.name != "Sulfuras, Hand of Ragnaros":
-                            item.quality = item.quality - 1
+                            self.decrease_quality(item)
                 else:
                     item.quality = item.quality - item.quality
             else:
                 if item.quality < 50:
-                    item.quality = item.quality + 1
+                    self.increase_quality(item)
